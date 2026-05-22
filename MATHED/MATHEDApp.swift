1b0d92215b95cda1fc89a8b9322557f862ea14fb
Sahil <sahiltripurana@gmail.com>
2026-05-22 18:43:36 -0500

    Initial Commit

---
+++ MATHED/MATHEDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MATHEDApp.swift
+//  MATHED
+//
+//  Created by Sahil tripurana on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct MATHEDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MATHEDTests/MATHEDTests.swift
@@ -0,0 +1,18 @@
+//
+//  MATHEDTests.swift
+//  MATHEDTests
+//
+//  Created by Sahil tripurana on 5/22/26.
+//
+
+import Testing
+
+struct MATHEDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
